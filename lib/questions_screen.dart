35e8ec6e3bbc5f1b50fd6491da66593fd31fc197
JoshuaAB5 <joshuaalexander.baeza@calbaptist.edu>
2025-09-23 11:54:30 -0700

    First Wrap test

--- lib/questions_screen.dart
+++ lib/questions_screen.dart
@@ -31,9 +31,12 @@ class _QuestionsScreenState extends State<QuestionsScreen> {
     return SizedBox(
       width: double.infinity,
       child: Container(
+        width: MediaQuery.of(context).size.width,
+        height: MediaQuery.of(context).size.height,
         margin: const EdgeInsets.all(40),
         child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
+          mainAxisAlignment:
+              MainAxisAlignment.center, // to center the content vertically
           crossAxisAlignment: CrossAxisAlignment.stretch,
           children: [
             Text(
@@ -58,5 +61,47 @@ class _QuestionsScreenState extends State<QuestionsScreen> {
         ),
       ),
     );
+
+    // return SizedBox(
+    //   width: double.infinity,
+    //   height: double.infinity,
+    //   child: Container(
+    //     width: MediaQuery.of(context).size.width * 0.9,
+    //     height: MediaQuery.of(context).size.height * 0.9,
+    //     margin: const EdgeInsets.all(40),
+    //     child: Wrap(
+    //       // direction: Axis.vertical,
+    //       runAlignment: WrapAlignment.center, // Centers the content vertically
+    //       alignment:
+    //           WrapAlignment.center, // Centers the button content horizontally
+
+    //       // crossAxisAlignment: WrapCrossAlignment.center,
+    //       spacing: 20, // side spacing between buttons
+    //       runSpacing: 20, // spacing between lines of buttons
+    //       // mainAxisAlignment: MainAxisAlignment.center,      // to center the content vertically
+    //       // crossAxisAlignment: CrossAxisAlignment.stretch,   // to make buttons full width
+    //       children: <Widget>[
+    //         Text(
+    //           textAlign: TextAlign.center,
+    //           currentQuestion.text,
+    //           style: GoogleFonts.lato(
+    //             color: const Color.fromARGB(255, 201, 153, 251),
+    //             fontSize: 24,
+    //             fontWeight: FontWeight.bold,
+    //           ),
+    //         ),
+    //         const SizedBox(height: 30),
+    //         ...currentQuestion.shuffledAnswers.map((answer) {
+    //           return AnswerButton(
+    //             answerText: answer,
+    //             onTap: () {
+    //               answerQuestion(answer);
+    //             },
+    //           );
+    //         }),
+    //       ],
+    //     ),
+    //   ),
+    // );
   }
 }
